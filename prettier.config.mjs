e108ee2545ea6170566057c31ff9ff5a373499d2
yechao.zhang <yechao.zhang@shopee.com>
2026-01-03 21:31:21 +0800

    feat: add prettier for tailwind

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+import * as tailwindPlugin from 'prettier-plugin-tailwindcss';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  plugins: [tailwindPlugin],
+};
